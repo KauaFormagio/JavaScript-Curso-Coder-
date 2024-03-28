deac153c1efcc30bb5d297f8b107df6415a8ebbc
KauaFormagio <kauavini01@gmail.com>
2024-03-28 12:42:58 -0300

    Aula 02

---
+++ Aula02/Exercicio/script.js
@@ -0,0 +1,22 @@
+//Variáveis setadas
+var ola = "Olá, ";
+var num1 = 5;
+
+//Pedido do Nome do Usuario
+var nome = prompt("Insira seu nome");
+
+//Resultado mostrado como alerta na página
+var saudacao = ola + nome;
+alert(saudacao);
+
+//Pedido de qualquer número e alteração de string para valor númerico
+var numString = prompt("Insira um número");
+var num2 = parseFloat(numString);
+
+//soma e multiplicação dos números
+var soma = num1 + num2;
+var mult = num1 * num2;
+
+//Resultado mostrado através do console
+console.log(soma);
+console.log(mult);
